e7517e61381d91ee2b79de2200f984db13ef9330
Nikolayz <zans55@yandex.ru>
2023-02-16 16:01:08 +0300

    56

---
+++ 56/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу копирования массива
+int [] arr = {15,8,9,3,10};
+void Copy()
+{ for (int i=0; i!=arr.Length;i++)
+{ int[] arr2;
+   arr2= new int[arr.Length];
+    arr2[i]=arr[i];
+System.Console.WriteLine($"{arr2[i]}");
+}
+}
+Copy();
