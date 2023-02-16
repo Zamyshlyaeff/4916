1d5b4db543b90a5ed1107b971ddd5aa6f1163710
Nikolayz <zans55@yandex.ru>
2023-02-16 15:58:40 +0300

    51

---
+++ 51/Program.cs
@@ -0,0 +1,21 @@
+﻿/* С клавиатуры вводится число N.
+Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры*/
+int N=0, c=0;
+int Init()
+{
+N=Convert.ToInt32(Console.ReadLine());
+return N;
+}
+int Schet(int a)
+{ for (int i=0;i<a;i++)
+ {
+    int b=Convert.ToInt32(System.Console.ReadLine());
+    if (b>0)
+       {c++;}
+   
+ }
+return c;
+}
+Init();
+Schet(N);
+ System.Console.WriteLine($"{c}");
